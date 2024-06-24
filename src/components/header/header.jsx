aed75637c856ed1ee89b48ef2e5bfa69a37fef0c
GMaxDev <maxime.guigra@gmail.com>
2024-06-24 10:56:12 +0200

    style: update with img and positioning

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,16 @@
+import Logo from "../../img/argentBankLogo.png";
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
-    <>
-      <h1 className="text-xl">Header</h1>
-    </>
+    <div className="flex items-center justify-between px-5 py-1 h-14">
+      <Link to="/">
+        <img className="h-14" src={Logo} alt="Argent Bank Logo" />
+      </Link>
+
+      <Link to="/" className="text-xl">
+        <i className="fa fa-user-circle"></i> Sign In
+      </Link>
+    </div>
   );
 }
